9e549c2db947428c22b759f5eeab112a66d17c1a
Alberto Jimenez <albertojimenez72@hotmail.com>
2020-02-08 20:25:56 +0100

    Implemented the user interface (console) to behave as asked

--- WordSearchEngine/WordSearchEngine/Program.cs
+++ WordSearchEngine/WordSearchEngine/Program.cs
@@ -4,9 +4,39 @@ namespace WordSearchEngine
 {
 	class Program
 	{
+		private static readonly IDirectoryManager directoryManager = new DirectoryManager(10);
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			if (args.Length == 0)
+			{
+				Console.WriteLine("Please add a directory path as argument");
+			}
+			else
+			{
+				var directoryToLoad = args[0];
+				var numberOfFilesLoaded = directoryManager.LoadFilesFromFolder(directoryToLoad);
+				Console.WriteLine($"{numberOfFilesLoaded} files read in directory {directoryToLoad}");
+				Console.WriteLine();
+				ShowSearchOption();
+			}
+		}
+
+		static void ShowSearchOption()
+		{
+			var goOn = true;
+			while (goOn)
+			{
+				Console.Write("Search> ");
+				var wordToFind = Console.ReadLine();
+				if (wordToFind != "")
+				{
+					var result = directoryManager.GetTopSearchWordMatches(wordToFind);
+					Console.WriteLine(result);
+				}
+				else
+					goOn = false;
+			}
 		}
 	}
 }
