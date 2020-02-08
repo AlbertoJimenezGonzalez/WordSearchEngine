a09196660874341f91e206ea799f01b9ee3269fe
Alberto Jimenez <albertojimenez72@hotmail.com>
2020-02-08 19:36:55 +0100

    DirectoryManager and IDirectoryManager created

---
+++ WordSearchEngine/WordSearchEngine/DirectoryManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordSearchEngine
+{
+	public class DirectoryManager : IDirectoryManager
+	{
+		private readonly List<ITextFileForSearch> filesInFolder;
+
+		public DirectoryManager()
+		{
+			filesInFolder = new List<ITextFileForSearch>();
+		}
+
+		public int LoadFilesFromFolder(string pathToFolder)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string GetTopSearchWordMatches(string wordToSearch)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				filesInFolder.Clear();
+			}
+		}
+	}
+}
---
+++ WordSearchEngine/WordSearchEngine/IDirectoryManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordSearchEngine
+{
+	public interface IDirectoryManager : IDisposable
+	{
+		int LoadFilesFromFolder(string pathToFolder);
+		string GetTopSearchWordMatches(string wordToSearch);
+
+	}
+}
